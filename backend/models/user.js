214e352a59adfe507dad64ec4333b74536662af6
natolay <ndixon@seattleu.edu>
2022-12-11 17:27:24 +0100

    created a username schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -10,3 +10,13 @@ const workoutSchema = new Schema({
 
 const Exercise = mongoose.model('Exercise', workoutSchema);
 module.exports = Exercise;
+
+const userSchema = new Schema ({
+    username: {type:String,
+    required: true,
+minlength:5}
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
