92cfa9815b09885c1ea7e53fe5bfcc2583fc9109
natolay <ndixon@seattleu.edu>
2022-12-11 17:17:24 +0100

    connected mongoose

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,31 @@ const mongoose = require('mongoose');
 const router = require('./router/router');
 const ejs = require('ejs');
 const path = require('path');
+const { Server } = require('http');
 require('dotenv').config();
-
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 const DB_URL = `mongodb+srv://${MONGO_PASS}@todolist.sweq1il.mongodb.net/?retryWrites=true&w=majority`
 
-const app = express();
+mongoose
+    .connect(DB_URL, {
+        useNewUrlParser: true, useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('We are connected to MongoDB')
+    })
+
 
 app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}))
 
 
 app.get('/exercise', (req,res))
 
 
-app.listen(3000, function() {
-    console.log("listening on PORT 3000")
+app.listen(PORT, async() {
+    console.log(`Server is up on port ${PORT}`)
 })
 
