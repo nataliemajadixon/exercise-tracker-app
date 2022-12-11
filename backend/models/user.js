5ca3e69c1f38b8ec8c196a2655accd344dd78f93
natolay <ndixon@seattleu.edu>
2022-12-11 17:23:58 +0100

    exported workout schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const workoutSchema = new Schema({
     username: {type: String, required: true},
-    description: {type:String, required: true},
-    
+    description: {type: String, required: true},
+    date: {type: Date, required:true}
 })
+
+const Exercise = mongoose.model('Exercise', workoutSchema);
+module.exports = Exercise;
