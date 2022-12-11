88a69d84e88de4a1d190fcf281da7af5000a1c4a
natolay <ndixon@seattleu.edu>
2022-12-11 17:13:48 +0100

    updated server js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const cors = require('cors');
+const mongoose = require('mongoose');
+const router = require('./router/router');
+const ejs = require('ejs');
+const path = require('path');
+require('dotenv').config();
 
+
+const PORT = process.env.PORT || 3000;
+
+const DB_URL = `mongodb+srv://${MONGO_PASS}@todolist.sweq1il.mongodb.net/?retryWrites=true&w=majority`
 
 const app = express();
 
